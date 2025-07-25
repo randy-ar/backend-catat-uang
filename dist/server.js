8c6a32df31b0029e601cdeae27ef9d4582ec6460
randy-ar <randyabdulrahman87@gmail.com>
2025-07-25 14:27:51 +0700

    fix firebase config

--- dist/server.js
+++ dist/server.js
@@ -4,7 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // server.ts
+const child_process_1 = require("child_process");
 const app_1 = __importDefault(require("./app"));
+(0, child_process_1.exec)('npm run generate-service', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+    }
+    console.log(stdout);
+    console.log(stderr);
+});
 const PORT = process.env.PORT || 5000;
 app_1.default.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- server.ts
+++ server.ts
@@ -1,5 +1,14 @@
 // server.ts
+import { exec } from 'child_process';
 import app from './app';
+exec('npm run generate-service', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  console.log(stdout);
+  console.log(stderr);
+});
 
 const PORT = process.env.PORT || 5000;
 
