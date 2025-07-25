466db82e415ada5b2d44f624630b81396641e010
randy-ar <randyabdulrahman87@gmail.com>
2025-07-25 20:44:26 +0700

    fix ocr services

--- src/services/ocrService.ts
+++ src/services/ocrService.ts
@@ -2,9 +2,7 @@
 import Tesseract, { OEM } from "tesseract.js";
 
 const performOcr = async (imagePathOrBuffer: string | Buffer): Promise<string> => {
-  const worker = await Tesseract.createWorker('ind', {} as OEM, {
-    corePath: 'dist/tesseract.js-core',
-  });
+  const worker = await Tesseract.createWorker('ind');
   const ret = await worker.recognize(imagePathOrBuffer);
   await worker.terminate();
   return ret.data.text;
